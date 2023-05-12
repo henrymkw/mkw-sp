234beb0435943a4b00fdb9f85eb8319839d62746
Zachruff <zachary.rubin02@gmail.com>
2023-05-12 12:21:20 -0400

    wip 1

--- payload/game/ui/SettingsPage.hh
+++ payload/game/ui/SettingsPage.hh
@@ -70,12 +70,17 @@ private:
     u32 getSheetCount() const;
     CategoryInfo getCategoryInfo(u32 sheetIndex) const;
 
+    void onSettingsWheelButtonFront(PushButton *button, u32 localPlayerId);
+    void onSettingsWheelButtonSelect(PushButton *button, u32 localPlayerId);
+    void onSettingsWheelButtonDeselect(PushButton *button, u32 localPlayerId);
+
     template <typename T>
     using H = typename T::template Handler<SettingsPage>;
 
     template <typename T>
     using C = typename T::template ChangeHandler<SettingsPage>;
 
+    //UI elements
     MultiControlInputManager m_inputManager;
     CtrlMenuPageTitleText m_pageTitleText;
     UpDownControl m_categoryControl;
@@ -83,6 +88,11 @@ private:
     UpDownControl m_settingControls[6];
     TextUpDownValueControl m_settingValues[6];
     CtrlMenuBackButton m_backButton;
+
+//Array should work for this?
+    PushButton m_settingsWheelButton[5];
+
+    //Input handlers
     H<MultiControlInputManager> m_onBack{this, &SettingsPage::onBack};
     H<UpDownControl> m_onCategoryControlFront{this, &SettingsPage::onCategoryControlFront};
     H<UpDownControl> m_onCategoryControlSelect{this, &SettingsPage::onCategoryControlSelect};
@@ -91,6 +101,12 @@ private:
     H<UpDownControl> m_onSettingControlFront{this, &SettingsPage::onSettingControlFront};
     H<UpDownControl> m_onSettingControlSelect{this, &SettingsPage::onSettingControlSelect};
     H<PushButton> m_onBackButtonFront{this, &SettingsPage::onBackButtonFront};
+
+//Figure out how this works with an array of pushbuttons.
+    H<PushButton> m_onSettingsWheelButtonFront{this, &SettingsPage::onSettingsWheelButtonFront};
+    H<PushButton> m_onSettingsWheelButtonSelect{this, &SettingsPage::onSettingsWheelButtonSelect};
+    H<PushButton> m_onSettingsWheelButtonDeselect{this, &SettingsPage::onSettingsWheelButtonDeselect};
+
 
 protected:
     IHandler *m_handler = nullptr;
