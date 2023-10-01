44b2ece6f0f692a4e7991049d277c15d514373ea
henrymkw <86250009+henrymkw@users.noreply.github.com>
2023-10-01 13:47:11 -0500

    fixed formatting

--- payload/game/item/ItemSlotTable.cc
+++ payload/game/item/ItemSlotTable.cc
@@ -6,9 +6,8 @@
 namespace Item {
 
 void ItemSlotTable::resetPowTimer() {
-    // auto saveManager = System::SaveManager::Instance();
-    // auto hopDodge = saveManager->getSetting<SP::ClientSettings::HopDodgePractice>();
-    if (System::SaveManager::Instance()->getSetting<SP::ClientSettings::Setting::HopDodgePractice>() ==
+    if (System::SaveManager::Instance()
+                    ->getSetting<SP::ClientSettings::Setting::HopDodgePractice>() ==
             SP::ClientSettings::HopDodgePractice::Enable) {
         powTimer = 60;
     } else {
